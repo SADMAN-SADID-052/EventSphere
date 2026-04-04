c657a9a51affb1eec0feb26e19829daa8decbebb
SADMAN-SADID-052 <sadman.cse8.bu@gmail.com>
2026-04-05 00:05:03 +0600

    add toaster in the layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
+import { Toaster } from "react-hot-toast";
 
 export const metadata: Metadata = {
   title: "EventSphere",
@@ -17,6 +18,31 @@ export default function RootLayout({
     <html lang="en">
       <body className="bg-[#0b0b0f] text-white">
         <Navbar />
+        <Toaster
+          position="top-right"
+          toastOptions={{
+            style: {
+              background: "#13131a",
+              color: "#fff",
+              border: "1px solid rgba(255,255,255,0.1)",
+              borderRadius: "12px",
+              fontSize: "14px",
+            },
+            success: {
+              iconTheme: {
+                primary: "#605DFF",
+                secondary: "#fff",
+              },
+            },
+            error: {
+              iconTheme: {
+                primary: "#E85D24",
+                secondary: "#fff",
+              },
+            },
+          }}
+        />
+
         {children}
         <Footer></Footer>
       </body>
